862045cac7155435c366dd0a7f1aff75c108c182
randombreadz <83088858+randombreadz@users.noreply.github.com>
2022-04-07 18:46:17 -0700

    Add files via upload

---
+++ Abc.py
@@ -0,0 +1,18 @@
+
+def __init__(self, model, color, company, speed_limit):
+    self.color = color
+    self.model = model
+    self.company = company
+    self.speed_limit = speed_limit
+
+def start(self):
+    print("Start it")
+
+def stop(self):
+    print("Stop it")
+
+def accelerate(self):
+    print("Accelerating")
+
+def change_gear(self):
+    print("Gear change")
